1339b318735cb852f8729bd589e149a379545c36
michaelgruenstaeudl <gruenstaeudl.1@osu.edu>
2015-06-02 13:16:09 +0200

    2015.06.02.1300

--- CommT.Jan2015_DotPlots_function.R
+++ CommT.Jan2015_DotPlots_function.R
@@ -9,17 +9,17 @@
 ####################
 # Global Variables #
 ####################
-#in_folder = "/run/media/michael/6974AEDF6DB4DD0C/05_Project_CommT_Jan2015/01_starBEAST_GeneTrees/07_parsed_results/"
-#out_folder = "/run/media/michael/6974AEDF6DB4DD0C/05_Project_CommT_Jan2015/01_starBEAST_GeneTrees/08_visualization/"
+#in_folder = "/run/media/michael/6974AEDF6DB4DD0C/05_Project_CommT_Jan2015/02_starBEAST_GeneTrees/07_parsed_results/"
+#out_folder = "/run/media/michael/6974AEDF6DB4DD0C/05_Project_CommT_Jan2015/02_starBEAST_GeneTrees/08_visualizations/"
 
 #n_sims = 20
 #n_genes_fixed = 10
 #n_sum_stats = 3
 #nums = sprintf("%02d", c(1:n_sims))
-#fn_prefixes = c("noswap", "g1swap", "g1a2swap", "g1t4swap")
+#fn_prefixes = c("noswap", "g1swap", "g1a2swap", "g1t4swap", "gAllShuf")
 
 
-dotPlots = function (n_genes_fixed, n_sum_stats, nums, fn_prefix) {
+dotPlots = function (n_genes_fixed, n_sum_stats, nums, fn_prefix, alpha_level, selected_metric) {
 
     #############
     # Libraries #
@@ -28,7 +28,7 @@ dotPlots = function (n_genes_fixed, n_sum_stats, nums, fn_prefix) {
 
     ## Load .rda-files
     for (num in nums) {
-          load(paste(in_folder, "CommT.Jan2015.", fn_prefix, ".sim.", num, ".rda", sep=""))
+      load(paste(in_folder, "CommT.Jan2015.", fn_prefix, ".sim.", num, ".rda", sep=""))
     }
 
     ####################
@@ -74,38 +74,41 @@ dotPlots = function (n_genes_fixed, n_sum_stats, nums, fn_prefix) {
     ######################################## 
 
     # perGene #
-    dataHandle_a0.05_perGene = list()
+    dataHandle_perGene = list()
     for (num in nums) {
-      name_handle = paste("CommT.Jan2015.", fn_prefix, ".sim.", num, "$results$alpha0.05$perGene", sep="")
-      dataHandle_a0.05_perGene[[num]] = eval(parse(text = name_handle))
+      name_handle = paste("CommT.Jan2015.", fn_prefix, ".sim.", num, "$results$alpha", alpha_level, "$perGene", sep="")
+      # TFL is necessary because data under "gAllShuf" is saved as "CommT.", not as "CommT.Jan2015."
+      if (fn_prefix == "gAllShuf") {name_handle = paste("CommT.", fn_prefix, ".sim.", num, "$results$alpha", alpha_level, "$perGene", sep="")}
+      dataHandle_perGene[[num]] = eval(parse(text = name_handle))
     }
     # acrGenes #
-    dataHandle_a0.05_acrGene = list()
+    dataHandle_acrGene = list()
     for (num in nums) {
-      name_handle = paste("CommT.Jan2015.", fn_prefix, ".sim.", num, "$results$alpha0.05$acrGene", sep="")
-      dataHandle_a0.05_acrGene[[num]] = eval(parse(text = name_handle))
+      name_handle_1 = paste("CommT.Jan2015.", fn_prefix, ".sim.", num, "$results$alpha", alpha_level, "$acrGene", sep="")
+      if (fn_prefix == "gAllShuf") {name_handle = paste("CommT.", fn_prefix, ".sim.", num, "$results$alpha", alpha_level, "$acrGene", sep="")}
+      dataHandle_acrGene[[num]] = eval(parse(text = name_handle))
     }
 
     ######################
     # STEP2. Format data #
     ######################
 
-    # perGene - 5 loci at alpha=0.05 #
-    dataHandle_pG_0.05 = do.call("rbind", wrapper(dataHandle_a0.05_perGene, nums, n_genes_fixed))
-    # acrGenes - 5 loci at alpha=0.05 #
-    dataHandle_aG_0.05 = do.call("rbind", wrapper(dataHandle_a0.05_acrGene, nums, n_genes_fixed, special=T))
+    # perGene #
+    dataHandle_pG = do.call("rbind", wrapper(dataHandle_perGene, nums, n_genes_fixed))
+    # acrGenes #
+    dataHandle_aG = do.call("rbind", wrapper(dataHandle_acrGene, nums, n_genes_fixed, special=T))
 
-    #dataHandle_0.05 = rbind(dataHandle_pG_0.05, dataHandle_aG_0.05)
+    #dataHandle = rbind(dataHandle_pG, dataHandle_aG)
 
     ####################################
     # STEP3. Extract relevant sections #
     ####################################
 
     # Only observe pergene values
-    dataHandle_0.05 = dataHandle_pG_0.05
+    dataHandle = dataHandle_pG
 
     # Extract ndc values
-    out_data = dataHandle_0.05[which(dataHandle_0.05[,2]=="ndc"),]
+    out_data = dataHandle[which(dataHandle[,2]==selected_metric),]
 
     # convert to characters for better plotting
     out_data[,'gene'] = as.character(out_data[,'gene'])
@@ -114,23 +117,23 @@ dotPlots = function (n_genes_fixed, n_sum_stats, nums, fn_prefix) {
     # STEP4. Make plots #
     #####################
 
-    my_plot = ggplot(data=out_data, aes(x=sim, y=gene)) +
-        geom_point(aes(colour=value), size=3, alpha=1.0) +
-        scale_colour_manual(values=c(NA, 'black')) +
-        facet_grid(stat ~ .) +
-        ggtitle(paste(fn_prefix, ", alpha = 0.05", "\n", sep="")) +
-        theme_bw() +
-        scale_x_discrete(breaks=c(nums), labels=c(nums)) +
-        #scale_y_discrete(limits=c("cv", "mode", "median", "mean", "sum", c(n_genes_fixed:1))) +
-        scale_y_discrete(limits=as.character(c(n_genes_fixed:1))) +
-        theme(legend.position = "none",
-              axis.text = element_text(size=8),
-              strip.background=element_rect(fill="white")) +
-        xlab("\nSimulations") + 
-        ylab("Genes\n")
-
-    ggsave(filename=paste(out_folder, fn_prefix, "_0.05.svg", sep=""),
-           plot=my_plot, width=5, height=3)
+#    my_plot = ggplot(data=out_data, aes(x=sim, y=gene)) +
+#        geom_point(aes(colour=value), size=3, alpha=1.0) +
+#        scale_colour_manual(values=c(NA, 'black')) +
+#        facet_grid(stat ~ .) +
+#        ggtitle(paste(fn_prefix, ", alpha = ", alpha_level, "\n", sep="")) +
+#        theme_bw() +
+#        scale_x_discrete(breaks=c(nums), labels=c(nums)) +
+#        #scale_y_discrete(limits=c("cv", "mode", "median", "mean", "sum", c(n_genes_fixed:1))) +
+#        scale_y_discrete(limits=as.character(c(n_genes_fixed:1))) +
+#        theme(legend.position = "none",
+#              axis.text = element_text(size=8),
+#              strip.background=element_rect(fill="white")) +
+#        xlab("\nSimulations") + 
+#        ylab("Genes\n")
+#
+#    ggsave(filename=paste(out_folder, fn_prefix, "_", alpha_level, ".svg", sep=""),
+#           plot=my_plot, width=5, height=3)
 
     ######################
     # STEP5. Return data #
--- CommT.Jan2015_DotPlots_wrapper.R
+++ CommT.Jan2015_DotPlots_wrapper.R
@@ -13,42 +13,55 @@ require(ggplot2)
 ####################
 # Global Variables #
 ####################
-in_folder = "/run/media/michael/6974AEDF6DB4DD0C/05_Project_CommT_Jan2015/01_starBEAST_GeneTrees/07_parsed_results/"
-out_folder = "/run/media/michael/6974AEDF6DB4DD0C/05_Project_CommT_Jan2015/01_starBEAST_GeneTrees/08_visualization/"
+#in_folder = "/run/media/michael/6974AEDF6DB4DD0C/05_Project_CommT_Jan2015/02_BEAST_GeneTrees/07_parsed_results/"
+#out_folder = "/run/media/michael/6974AEDF6DB4DD0C/05_Project_CommT_Jan2015/02_BEAST_GeneTrees/08_visualizations/"
 
+#in_folder = "/home/michael/Desktop/CommT_Visualization/02_BEAST_GeneTrees/"
+#out_folder = "/home/michael/Desktop/CommT_Visualization/output/"
+
+in_folder = "~/TEMP/input/"
+out_folder = "~/TEMP/output/"
+
+selected_metric = "ndc"
+alpha_level = "0.05"
 n_sims = 20
 n_genes_fixed = 10
 n_sum_stats = 3
 nums = sprintf("%02d", c(1:n_sims))
-#fn_prefixes = c("noswap", "g1swap", "g1a2swap", "g1t4swap")
+#fn_prefixes = c("noswap", "g1swap", "g1a2swap", "g1t4swap", "gAllShuf")
 
 
-dataHandle_0.05_noswap = dotPlots(n_genes_fixed, n_sum_stats, nums, "noswap")
-dataHandle_0.05_noswap[, ncol(dataHandle_0.05_noswap)+1] = "noswap"
-colnames(dataHandle_0.05_noswap)[ncol(dataHandle_0.05_noswap)] = "sim_type"
+dataHandle_noswap = dotPlots(n_genes_fixed, n_sum_stats, nums, "noswap", alpha_level, selected_metric)
+dataHandle_noswap[, ncol(dataHandle_noswap)+1] = "noswap"
+colnames(dataHandle_noswap)[ncol(dataHandle_noswap)] = "sim_type"
 
-dataHandle_0.05_g1swap = dotPlots(n_genes_fixed, n_sum_stats, nums, "g1swap")
-dataHandle_0.05_g1swap[, ncol(dataHandle_0.05_g1swap)+1] = "g1swap"
-colnames(dataHandle_0.05_g1swap)[ncol(dataHandle_0.05_g1swap)] = "sim_type"
+dataHandle_g1swap = dotPlots(n_genes_fixed, n_sum_stats, nums, "g1swap", alpha_level, selected_metric)
+dataHandle_g1swap[, ncol(dataHandle_g1swap)+1] = "g1swap"
+colnames(dataHandle_g1swap)[ncol(dataHandle_g1swap)] = "sim_type"
 
-dataHandle_0.05_g1a2swap = dotPlots(n_genes_fixed, n_sum_stats, nums, "g1a2swap")
-dataHandle_0.05_g1a2swap[, ncol(dataHandle_0.05_g1a2swap)+1] = "g1a2swap"
-colnames(dataHandle_0.05_g1a2swap)[ncol(dataHandle_0.05_g1a2swap)] = "sim_type"
+dataHandle_g1a2swap = dotPlots(n_genes_fixed, n_sum_stats, nums, "g1a2swap", alpha_level, selected_metric)
+dataHandle_g1a2swap[, ncol(dataHandle_g1a2swap)+1] = "g1a2swap"
+colnames(dataHandle_g1a2swap)[ncol(dataHandle_g1a2swap)] = "sim_type"
 
-dataHandle_0.05_g1t4swap = dotPlots(n_genes_fixed, n_sum_stats, nums, "g1t4swap")
-dataHandle_0.05_g1t4swap[, ncol(dataHandle_0.05_g1t4swap)+1] = "g1t4swap"
-colnames(dataHandle_0.05_g1t4swap)[ncol(dataHandle_0.05_g1t4swap)] = "sim_type"
+dataHandle_g1t4swap = dotPlots(n_genes_fixed, n_sum_stats, nums, "g1t4swap", alpha_level, selected_metric)
+dataHandle_g1t4swap[, ncol(dataHandle_g1t4swap)+1] = "g1t4swap"
+colnames(dataHandle_g1t4swap)[ncol(dataHandle_g1t4swap)] = "sim_type"
 
-dataHandle_0.05 = rbind(dataHandle_0.05_noswap, dataHandle_0.05_g1swap,
-                        dataHandle_0.05_g1a2swap, dataHandle_0.05_g1t4swap)
+dataHandle_gAllShuf = dotPlots(n_genes_fixed, n_sum_stats, nums, "gAllShuf", alpha_level, selected_metric)
+dataHandle_gAllShuf[, ncol(dataHandle_gAllShuf)+1] = "gAllShuf"
+colnames(dataHandle_gAllShuf)[ncol(dataHandle_gAllShuf)] = "sim_type"
 
-dataHandle_0.05[, 'sim_type'] = factor(dataHandle_0.05[, 'sim_type'], levels = c("noswap", "g1swap", "g1a2swap", "g1t4swap"))
+dataHandle = rbind(dataHandle_noswap, dataHandle_g1swap,
+                   dataHandle_g1a2swap, dataHandle_g1t4swap,
+                   dataHandle_gAllShuf)
 
-my_plot = ggplot(data=dataHandle_0.05, aes(x=sim, y=gene)) +
+dataHandle[, 'sim_type'] = factor(dataHandle[, 'sim_type'], levels = c("noswap", "g1swap", "g1a2swap", "g1t4swap", "gAllShuf"))
+
+my_plot = ggplot(data=dataHandle, aes(x=sim, y=gene)) +
     geom_point(aes(colour=value), size=3, alpha=1.0) +
     scale_colour_manual(values=c(NA, 'black')) +
     facet_grid(sim_type ~ .) +
-    ggtitle(paste("Combined, alpha = 0.05", "\n", sep="")) +
+    ggtitle(paste(selected_metric, ", alpha = ", alpha_level, "\n", sep="")) +
     theme_bw() +
     scale_x_discrete(breaks=c(nums), labels=c(nums)) +
     #scale_y_discrete(limits=c("cv", "mode", "median", "mean", "sum", c(n_genes_fixed:1))) +
@@ -59,5 +72,5 @@ my_plot = ggplot(data=dataHandle_0.05, aes(x=sim, y=gene)) +
     xlab("\nSimulations") + 
     ylab("Genes\n")
 
-ggsave(filename=paste(out_folder, "Combined_0.05.svg", sep=""),
+ggsave(filename=paste(out_folder, selected_metric, "_", alpha_level,".svg", sep=""),
        plot=my_plot, width=5, height=10)
